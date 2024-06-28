c2bfab0b2517f2c3ce1ae2b063bebaa96c3a7863
Фаина <petrenkolarisa260@gmail.com>
2024-06-28 12:34:16 +0300

    game

--- My file.py
+++ My file.py
@@ -1,1 +1,10 @@
-print("Me")
+import random
+num = random.randint(1, 100)
+num1 = int(input("Enter your number"))
+while num1 != num:
+    if num1 < num:
+        print("Загадкове число більше")
+    elif num1 > num:
+        print("Загадкове число менше")
+    num1 = int(input("Enter your number"))
+print("Ви вгадали")
