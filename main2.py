3f19917b1eb99110f37026735d91cd6d2df4b582
Фаина <petrenkolarisa260@gmail.com>
2024-06-30 11:30:15 +0300

    task3 lists

---
+++ main2.py
@@ -0,0 +1,6 @@
+list = [-6,-5,-4,-3,-2,-1,132,456,0,0,1,4,4,-6,2,6,-7]
+for index, i in enumerate(list):
+    if i >0:
+        print("The first positive number is ", i)
+        print("Index is ", index)
+        break
